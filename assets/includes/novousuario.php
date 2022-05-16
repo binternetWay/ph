fb1be23bb9ac5c9cb1e6f3ad645cbde30901cedc
binternetWay <vinicius.gabriel@ptovedorway.com>
2022-05-16 14:58:34 -0300

    ok

--- assets/includes/novousuario.php
+++ assets/includes/novousuario.php
@@ -1,7 +1,7 @@
 
 <div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="primeira__senha" name="primeira__senha" placeholder="Escolha uma senha" required="required" /></div>
 <div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="segunda__senha" name="segunda__senha" placeholder="Confirmação de senha" required="required" /></div>
-<div class="form__line"><button type="submit">Iniciar sessão</button></div>
+<div class="form__line"><button type="submit">Cadastrar usuário</button></div>
 
 <style>
 .btn__buscar{
---
+++ assets/includes/usuarioexistente.php
@@ -0,0 +1,9 @@
+<div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="senha" name="senha" placeholder="Senha" required="required" /></div>
+<div class="form__line"><button type="submit">Iniciar sessão</button></div>
+
+<style>
+.btn__buscar{
+    display: none;
+}
+</style>
+
