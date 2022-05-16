f21811524742c2a7653cb8bdeb0822fbb7301b0a
binternetWay <vinicius.gabriel@ptovedorway.com>
2022-05-16 14:57:04 -0300

    ok

--- assets/includes/novousuario.php
+++ assets/includes/novousuario.php
@@ -1,9 +1,7 @@
 
-<form method="POST" action="/ph/assets/modal/fLogin.php">
-    <div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="primeira__senha" name="primeira__senha" placeholder="Escolha uma senha" required="required" /></div>
-    <div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="segunda__senha" name="segunda__senha" placeholder="Confirmação de senha" required="required" /></div>
-    <div class="form__line"><button type="submit">Iniciar sessão</button></div>
-</form>
+<div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="primeira__senha" name="primeira__senha" placeholder="Escolha uma senha" required="required" /></div>
+<div class="form__line"><i class="fa-solid fa-key"></i><input type="password" id="segunda__senha" name="segunda__senha" placeholder="Confirmação de senha" required="required" /></div>
+<div class="form__line"><button type="submit">Iniciar sessão</button></div>
 
 <style>
 .btn__buscar{
